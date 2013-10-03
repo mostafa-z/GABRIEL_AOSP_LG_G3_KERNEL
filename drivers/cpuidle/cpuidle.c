d3be919d480ba061490d29d4555deec11ae55257
Viresh Kumar <viresh.kumar@linaro.org>
2013-10-03 21:26:48 +0530

    cpuidle: reduce code duplication inside cpuidle_idle_call()
    We are doing this twice in cpuidle_idle_call() routine:
    	drv->states[next_state].flags & CPUIDLE_FLAG_TIMER_STOP
    
    Would be better if we actually store this in a local variable and
    use that. That reduces code duplication and likely makes this piece
    of code run faster (in case the compiler wasn't able to optimize it
    earlier)
    
    Acked-by: Daniel Lezcano <daniel.lezcano@linaro.org>
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: mostafa-z <mostafazarghami@gmail.com>

--- drivers/cpuidle/cpuidle.c
+++ drivers/cpuidle/cpuidle.c
@@ -129,6 +129,7 @@ int cpuidle_idle_call(void)
 	struct cpuidle_device *dev = __this_cpu_read(cpuidle_devices);
 	struct cpuidle_driver *drv;
 	int next_state, entered_state;
+	bool broadcast;
 
 	if (off || !initialized)
 		return -ENODEV;
@@ -153,7 +154,9 @@ int cpuidle_idle_call(void)
 	trace_power_start_rcuidle(POWER_CSTATE, next_state, dev->cpu);
 	trace_cpu_idle_rcuidle(next_state, dev->cpu);
 
-	if (drv->states[next_state].flags & CPUIDLE_FLAG_TIMER_STOP)
+	broadcast = !!(drv->states[next_state].flags & CPUIDLE_FLAG_TIMER_STOP);
+
+	if (broadcast)
 		clockevents_notify(CLOCK_EVT_NOTIFY_BROADCAST_ENTER,
 				   &dev->cpu);
 
@@ -165,7 +168,7 @@ int cpuidle_idle_call(void)
 
 	trace_power_end_rcuidle(dev->cpu);
 
-	if (drv->states[next_state].flags & CPUIDLE_FLAG_TIMER_STOP)
+	if (broadcast)
 		clockevents_notify(CLOCK_EVT_NOTIFY_BROADCAST_EXIT,
 				   &dev->cpu);
 
