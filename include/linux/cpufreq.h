bb65ebbcbf68029a1cc0f4be9e0a624d2323c0c9
mostafa-z <mostafazarghami@gmail.com>
2017-06-01 13:12:46 +0430

    CPUFREQ: add alucard governor
    Signed-off-by: mostafa-z <mostafazarghami@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -371,6 +371,9 @@ extern struct cpufreq_governor cpufreq_gov_userspace;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ONDEMAND)
 extern struct cpufreq_governor cpufreq_gov_ondemand;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_ondemand)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ALUCARD)
+extern struct cpufreq_governor cpufreq_gov_alucard;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_alucard)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_CONSERVATIVE)
 extern struct cpufreq_governor cpufreq_gov_conservative;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_conservative)
